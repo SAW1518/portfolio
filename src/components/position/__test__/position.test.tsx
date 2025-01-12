a0bc2ef329c06fb004e6ab6f576860a1c08ad503
SAW1518 <enriquepppsaw@gmail.com>
2025-01-12 00:36:22 -0600

    adding modal content

--- src/components/position/__test__/position.test.tsx
+++ src/components/position/__test__/position.test.tsx
@@ -20,7 +20,51 @@ describe('Position Component', () => {
       'Node.js',
       'REST APIs',
     ],
-    images: ['/UP.png', '/login.png', '/section-management.png', '/delete.png'],
+    images: [
+      {
+        companyName: 'EPAM Systems',
+        src: '/login.png',
+        titule: 'Working in sign in and sign up in Expedia grup',
+        urls: [
+          'https://www.expedia.com/login',
+          'https://www.vrbo.com/login?enable_login=true',
+          'https://www.hoteles.com/login',
+          'https://www.wotif.com/login',
+        ],
+        descripcion: 'Sign in no required *',
+      },
+      {
+        companyName: 'EPAM Systems',
+        src: '/UP.png',
+        titule: 'Working in account-profile in Expedia grup',
+        urls: [
+          'https://www.expedia.com/account',
+          'https://www.vrbo.com/account',
+          'https://www.hoteles.com/account',
+          'https://www.wotif.com/account',
+        ],
+        descripcion: 'Sign in required *',
+      },
+      {
+        companyName: 'EPAM Systems',
+        src: '/section-management.png',
+        titule: 'Working in Session management in Expedia grup',
+        urls: ['https://www.expedia.com/account-security?dvc_history=true'],
+        descripcion: 'Sign in required *',
+      },
+      {
+        companyName: 'EPAM Systems',
+        src: '/delete.png',
+        titule: 'Working in Delete account in Expedia grup',
+        urls: [
+          'https://www.expedia.com/user/delete',
+          'https://www.vrbo.com/user/delete',
+          'https://www.hoteles.com/user/delete',
+          'https://www.wotif.com/user/delete',
+        ],
+        descripcion: 'Sign in required *',
+      },
+    ],
   };
   it('should render elements', () => {
     const { getByText, getAllByRole } = render(<Position {...defaultProps} />);
