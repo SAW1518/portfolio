ca1df1f9dea4da7155ba080bf234259c44403d4b
SAW1518 <enriquepppsaw@gmail.com>
2025-01-26 00:59:54 -0600

    fix: unit test

--- src/pages/home/__test__/home.test.tsx
+++ src/pages/home/__test__/home.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import { beforeEach, describe, expect, it, vi } from 'vitest';
 import { Home } from 'src/pages';
 describe('Home Component', () => {
